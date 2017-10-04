8bf77f0c828f0568dad079faf987d528b1054b02
Edge Quintanilla <edgarq86@gmail.com>
2017-10-03 20:19:38 -0500

    intialization with layout

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() { 
+console.log("linked up");
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCXu2LaS7EoHy-tgmaC-vt7cwtJYbSMjNM",
+    authDomain: "bootcampdb.firebaseapp.com",
+    databaseURL: "https://bootcampdb.firebaseio.com",
+    projectId: "bootcampdb",
+    storageBucket: "bootcampdb.appspot.com",
+    messagingSenderId: "425847005611"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+})  
